7c8b409f693a0f25bb5a17dd31fbaea2208ed092
mokshanirgutti <mokshasai910@gmail.com>
2024-03-15 07:24:22 +0530

    identifiers

--- identifers.c
+++ identifers.c
@@ -1,21 +1,32 @@
-#include <string.h>
 #include <stdio.h>
-#include <stdlib.h>
-int main(){
-    char idt[100];
-    int i=0;
-    scanf("%s",&idt);
-    if ((idt[i] >= 'a' && idt[i] < 'z' ) || (idt[i] >= 'A' && idt[i] < 'Z' ))
-    {
-        for ( i = 1; i < strlen(idt); i++)
-        {
-            if (!((idt[i] >='a' && idt[i] <='z' )  || (idt[i] >='A' && idt[i] <='Z' )  ||  (idt[i] >='0' && idt[i] <='9' ) || (idt[i] == '_')))
-            {
-                printf("Not identifier");
-                exit(0);
-            }           
+#include <string.h>
+
+int main() {
+    char identifier[100];
+    int i = 0, cs, error = 0;
+    scanf("%s", identifier);
+    enum state { q1, q2, q3, qf };
+
+    if ((identifier[i] >= 'a' && identifier[i] <= 'z') || (identifier[i] >= 'A' && identifier[i] <= 'Z') || (identifier[i] == '_'))
+        cs = q1;
+    else{
+        error = 1;
+        cs= qf;
+    }
+
+    for (i = 1; i < strlen(identifier) && !error; i++) {
+        switch (cs) {
+            case q1:
+                if (!((identifier[i] >= 'a' && identifier[i] <= 'z') || (identifier[i] >= 'A' && identifier[i] <= 'Z') || (identifier[i] >= '0' && identifier[i] <= '9') || (identifier[i] == '_')))
+                    error = 1;
+                break;
         }
-        printf("identifier");
-    }  else printf("Not identifier");
+    }
+
+    if (!error)
+        printf("Valid identifier\n");
+    else
+        printf("Not a valid identifier\n");
+
     return 0;
 }
